cd5bd0acadb7f56cce5a88bea14c21d49e477a40
Gabriel Kubota <gabrielkhiroshi@gmail.com>
2023-10-08 04:05:28 -0300

    new attempt

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,9 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
   kit: {
     adapter: adapter(),
-    // files: {
-    //   serviceWorker: 'service-worker.js',
-    // }
+    files: {
+      serviceWorker: 'service-worker.js',
+    }
   },
   preprocess: vitePreprocess()
 };
